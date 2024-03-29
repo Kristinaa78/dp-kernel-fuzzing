d170f2deb354763f4143eb13679ad60c35c7ef97
Kristinaa78 <krishrebenarova@gmail.com>
2024-03-29 23:26:33 +0100

    feat: add usb_fuzzer dir

---
+++ linux-user/usb_fuzzer/sharedir/agent.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+>&2 echo "[i] agent.sh starting" | vmcall hcat
+# print the contents of the root dir (debugging purposes)
+ls -li /
+# print the loader.sh contents
+#>&2 cat /loader.sh
+>&2  echo "[i] currently in: $(pwd)"
+# set up USB testing device
+>&2  echo "[i] downloading sampleUSB" | vmcall hcat                                                               
+vmcall hget -x -o /fuzz sampleUSB
+cd /fuzz
+# print the contents of the /fuzz dir (debugging purposes)
+ls -li /fuzz
+>&2  echo "[+] starting the emulation" | vmcall hcat
+sampleUSB
+# return to loader.sh, which will upload agent.log
