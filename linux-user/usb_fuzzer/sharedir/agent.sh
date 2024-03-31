95ccd2498b4d5a0817d9da4fbc1ed4d03f8f05c6
Kristinaa78 <krishrebenarova@gmail.com>
2024-03-31 13:44:52 +0200

    [WIP]: kAFL handshake

--- linux-user/usb_fuzzer/sharedir/agent.sh
+++ linux-user/usb_fuzzer/sharedir/agent.sh
@@ -7,13 +7,15 @@ ls -li /
 #>&2 cat /loader.sh
 >&2 echo "[i] currently in: $(pwd)"
 # set up USB testing device
->&2 echo "[i] downloading sampleUSB" | vmcall hcat                                                               
-vmcall hget -x -o /fuzz sampleUSB
+# >&2 echo "[i] downloading sampleUSB" | vmcall hcat                                                               
+# vmcall hget -x -o /fuzz sampleUSB
 >&2 echo "[i] downloading fuzzer" | vmcall hcat
 vmcall hget -x -o /fuzz fuzzer
 cd /fuzz
 # print the contents of the /fuzz dir (debugging purposes)
 ls -li /fuzz
->&2 echo "[+] starting the emulation" | vmcall hcat
-sampleUSB
+# >&2 echo "[+] starting the emulation" | vmcall hcat
+# sampleUSB
+>&2 echo "[+] starting the fuzzing process" | vmcall hcat
+fuzzer
 # return to loader.sh, which will upload agent.log
--- linux-user/usb_fuzzer/src/fuzzer.c
+++ linux-user/usb_fuzzer/src/fuzzer.c
@@ -10,11 +10,47 @@
 #include <linux/usb/ch9.h>
 #include <linux/hid.h>
 
+#include <asm/nyx_api.h>
+
 #include "constants.h"
 #include "fuzzer.h"
 
 int setup_usb_device(int fd);
 int send_usb_request(int fd);
+
+// kAFL initialization function - follows the kAFL initialization protocol
+// [https://intellabs.github.io/kAFL/tutorials/linux/dvkm/agent.html#initialization]
+kAFL_payload* kafl_init()
+{
+    host_config_t host_config = {0};
+    hprintf("[i] kAFL agent initialization\n");
+    // 1. kAFL handshake
+    kAFL_hypercall(HYPERCALL_KAFL_ACQUIRE, 0);
+    kAFL_hypercall(HYPERCALL_KAFL_RELEASE, 0);
+    // 2. query host config
+    kAFL_hypercall(HYPERCALL_KAFL_GET_HOST_CONFIG, (uintptr_t)&host_config);
+    hprintf("[i] HOST CONFIG:\n");
+    hprintf("\thost_magic:          %d [0x%x]\n", host_config.host_magic, host_config.host_magic);
+    hprintf("\thost_version:        %d [0x%x]\n", host_config.host_version, host_config.host_version);
+    hprintf("\tbitmap_size:         %d [%dKB]\n", host_config.bitmap_size, host_config.bitmap_size / 1024);
+    hprintf("\tijon_bitmap_size:    %dB\n", host_config.ijon_bitmap_size);
+    hprintf("\tpayload_buffer_size: %d [%dKB]\n",
+            host_config.payload_buffer_size,
+            host_config.payload_buffer_size / 1024);
+    hprintf("\tworker_id:           %d\n", host_config.worker_id);
+    // 3. set guest agent config
+    //
+    // 4. allocate payload buffer
+    //
+    // 5. map payload buffer
+    //
+    // 6. submit crash handlers
+    //
+    // 7. submit Intel PT ranges
+    //
+    // 8. submit CR3
+    return NULL;
+}
 
 int main() {
     int fd;
@@ -23,6 +59,7 @@ int main() {
 	if(freopen(NULL, "w", stdout) == NULL) return -1;
 
     fprintf(stdout, "[i] fuzzer started\n");
+    kAFL_payload *payload = kafl_init(); 
     usb_init(fd, USB_SPEED_HIGH, "dummy_udc.0", "dummy_udc");
     fprintf(stdout, "[+] USB device initialized\n");
     usb_run(fd);
