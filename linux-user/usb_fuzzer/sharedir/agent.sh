1bbde7885fe8b3cd6e3fafe28c41aa823bb6af47
Kristinaa78 <krishrebenarova@gmail.com>
2024-04-28 18:13:20 +0200

    feat: update kafl config, refactor: makefile and agent.sh

--- linux-user/usb_fuzzer/sharedir/agent.sh
+++ linux-user/usb_fuzzer/sharedir/agent.sh
@@ -2,9 +2,15 @@
 
 >&2 echo "[i] agent.sh starting" | vmcall hcat
 
+# download the USB fuzzing device from host
 >&2 echo "[i] downloading fuzzer" | vmcall hcat
 vmcall hget -x -o /fuzz fuzzer
 cd /fuzz
+
+# check if kaslr is not set up
+>&2 echo "[i] cat /proc/kallsyms | grep 'usb_free_coherent':" | vmcall hcat
+cat /proc/kallsyms | grep "usb_free_coherent"
+
 >&2 echo "[+] starting the fuzzing process" | vmcall hcat
 fuzzer
 
